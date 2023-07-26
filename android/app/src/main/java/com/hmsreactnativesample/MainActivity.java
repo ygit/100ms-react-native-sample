bbb837dbcbe632a0d856bffd85437264d92e5b02
Yogesh Singh <yogesh.singh.2810@gmail.com>
2023-07-26 13:05:55 +0530

    updated Example app component name

--- android/app/src/main/java/com/hmsreactnativesample/MainActivity.java
+++ android/app/src/main/java/com/hmsreactnativesample/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNExample";
+    return "HMSReactNativeSample";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
